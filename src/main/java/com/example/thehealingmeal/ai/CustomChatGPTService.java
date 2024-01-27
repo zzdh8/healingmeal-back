acc84c89a5b63b2f5fefff59c863f4de0338e956
Hyun jinyong <ekwls20@gmail.com>
2024-01-27 20:18:16 +0900

    새로운 로직 작성

--- src/main/java/com/example/thehealingmeal/ai/CustomChatGPTService.java
+++ src/main/java/com/example/thehealingmeal/ai/CustomChatGPTService.java
@@ -1,18 +1,41 @@
 package com.example.thehealingmeal.ai;
 
+import com.example.thehealingmeal.ai.dto.AiResDto;
+import com.example.thehealingmeal.menu.api.dto.MenuResponseDto;
+import com.example.thehealingmeal.menu.api.dto.SnackOrTeaResponseDto;
+import com.example.thehealingmeal.menu.domain.Meals;
+import com.example.thehealingmeal.menu.service.MenuProvider;
 import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomChatGPTService {
     private final ChatgptService chatgptService;
+    private final MenuProvider menuProvider;
 
-   public String getAnswer() {
-       return chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user","how are you?")));
+    public AiResDto getAnswer(long user_id, Meals meals) {
+        MenuResponseDto menu = menuProvider.provide(user_id, meals);
+        List<String> names = List.of(menu.getMain_dish(),menu.getRice(),menu.getSideDishForUserMenu().toString());
+        StringBuilder sentence = new StringBuilder();
+        for (String name : names){
+            sentence.append(name);
+            if (names.iterator().hasNext()){
+                sentence.append(", ");
+            }
+        }
+        String multiChat = chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user", sentence.toString()+"을 먹는다고 했을 때 섭취하는 사람에게 어떤 효능이 있을까? 위 아래 문단의 잡설은 하지 말고 리스트만 보여줘.")));
+        return new AiResDto(multiChat);
+    }
+
+    public AiResDto getAnswerSnackOrTea(long user_id, Meals meals) {
+        SnackOrTeaResponseDto menu = menuProvider.provideSnackOrTea(user_id, meals);
+        String multiChat = chatgptService.multiChat(Arrays.asList(new MultiChatMessage("user", menu.getSnack_or_tea()+"을 먹는다고 했을 때 섭취하는 사람에게 어떤 효능이 있을까? 위 아래 문단의 잡설은 하지 말고 리스트만 보여줘.")));
+        return new AiResDto(multiChat);
     }
 }
