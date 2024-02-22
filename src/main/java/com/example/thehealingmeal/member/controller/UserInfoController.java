c5791ccc239246808b7b7ff7d5af7ab8823af09b
zzdh8 <ekwls20@gmail.com>
2024-02-23 03:46:07 +0900

    modified

--- src/main/java/com/example/thehealingmeal/member/controller/UserInfoController.java
+++ src/main/java/com/example/thehealingmeal/member/controller/UserInfoController.java
@@ -32,12 +32,12 @@ public class UserInfoController {
 
     //비밀번호 변경 password modified
     @PutMapping("/{user_id}/change/pwd")
-    public ResponseEntity<String> changePwd(@RequestBody PwdChangeDto pwdChangeDto, @PathVariable String user_id){
+    public ResponseEntity<String> changePwd(@RequestBody PwdChangeDto pwdChangeDto, @PathVariable String user_id) {
         try {
             userInfoModify.changePwd(pwdChangeDto, user_id);
             return new ResponseEntity<>("changing password is success.", HttpStatus.OK);
-        } catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>("changing password is failed. please, check your input value.", HttpStatus.BAD_REQUEST);
         }
-
+    }
 }
--- src/main/java/com/example/thehealingmeal/member/service/UserInfoModify.java
+++ src/main/java/com/example/thehealingmeal/member/service/UserInfoModify.java
@@ -3,7 +3,6 @@ package com.example.thehealingmeal.member.service;
 
 import com.example.thehealingmeal.member.domain.User;
 import com.example.thehealingmeal.member.dto.PwdChangeDto;
-import com.example.thehealingmeal.member.execption.InvalidPasswordException;
 import com.example.thehealingmeal.member.execption.InvalidUserException;
 import com.example.thehealingmeal.member.execption.MismatchException;
 import com.example.thehealingmeal.member.repository.UserRepository;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -26,22 +24,25 @@ public class UserInfoModify {
 
     //비밀번호 변경
     @Transactional
-    public void changePwd(PwdChangeDto pwdChangeDto, String user_id){
-            User user = userRepository.findById(Long.valueOf(user_id)).orElseThrow(()-> new InvalidUserException("user not found in the user list table."));
-            if(passwordEncoder.matches(pwdChangeDto.getNowPwd(),user.getPassword())){
-                user.setPassword(passwordEncoder.encode(pwdChangeDto.getChangePwd()));
-                userRepository.save(user);
-            } else{
-                throw new MismatchException("the password is mismatch.");
-            }
-    //임시 비밀번호 발행
-    protected String generateTemPwd(int length){
-
-        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        SecureRandom random = new SecureRandom();
-        return IntStream.range(0, length)
-                .map(i -> random.nextInt(chars.length()))
-                .mapToObj(randomIndex -> String.valueOf(chars.charAt(randomIndex)))
-                .collect(Collectors.joining());
+    public void changePwd(PwdChangeDto pwdChangeDto, String user_id) {
+        User user = userRepository.findById(Long.valueOf(user_id)).orElseThrow(() -> new InvalidUserException("user not found in the user list table."));
+        if (passwordEncoder.matches(pwdChangeDto.getNowPwd(), user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(pwdChangeDto.getChangePwd()));
+            userRepository.save(user);
+        } else {
+            throw new MismatchException("the password is mismatch.");
+        }
     }
+
+
+        //임시 비밀번호 발행
+        protected String generateTemPwd ( int length){
+
+            final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            SecureRandom random = new SecureRandom();
+            return IntStream.range(0, length)
+                    .map(i -> random.nextInt(chars.length()))
+                    .mapToObj(randomIndex -> String.valueOf(chars.charAt(randomIndex)))
+                    .collect(Collectors.joining());
+        }
 }
