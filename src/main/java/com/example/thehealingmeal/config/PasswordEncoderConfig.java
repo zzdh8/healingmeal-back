a94090497764b3d524f237a4d3bff3d9d1b20648
zzdh8 <ekwls20@gmail.com>
2024-02-24 18:26:25 +0900

    config class move to upper directory

--- src/main/java/com/example/thehealingmeal/config/PasswordEncoderConfig.java
+++ src/main/java/com/example/thehealingmeal/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.example.thehealingmeal.member.config;
+package com.example.thehealingmeal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
