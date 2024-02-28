0ad66b248862bf75a7c54e46269a2dca74334cae
Hyun jinyong <ekwls20@gmail.com>
2024-02-28 17:38:55 +0900

    ddl-auto -> auto

--- src/main/java/com/example/thehealingmeal/menu/api/SnackUrlAutoAPI.java
+++ src/main/java/com/example/thehealingmeal/menu/api/SnackUrlAutoAPI.java
@@ -1,20 +1,20 @@
-package com.example.thehealingmeal.menu.api;
-
-import com.example.thehealingmeal.menu.service.SnackUrlService;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class SnackUrlAutoAPI {
-    private final SnackUrlService service;
-
-    @PostConstruct
-    @GetMapping("save")
-    public String SaveUrl(){
-        service.urlSave();
-        return "Url is saved.";
-    }
-}
+//package com.example.thehealingmeal.menu.api;
+//
+//import com.example.thehealingmeal.menu.service.SnackUrlService;
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class SnackUrlAutoAPI {
+//    private final SnackUrlService service;
+//
+//    @PostConstruct
+//    @GetMapping("save")
+//    public String SaveUrl(){
+//        service.urlSave();
+//        return "Url is saved.";
+//    }
+//}
