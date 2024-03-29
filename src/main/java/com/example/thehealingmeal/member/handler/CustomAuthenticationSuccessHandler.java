f50c82334464b5392c7a5c948757ad37df442d1f
Hyun jinyong <ekwls20@gmail.com>
2024-03-29 17:40:09 +0900

    불필요한 세션 생성 코드 삭제

--- src/main/java/com/example/thehealingmeal/member/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/thehealingmeal/member/handler/CustomAuthenticationSuccessHandler.java
@@ -3,7 +3,6 @@ package com.example.thehealingmeal.member.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
